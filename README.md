0648347c38aae2ee727e45e500316cd78dd0c728
Miguel Rust <hava12soft@gmail.com>
2018-06-12 08:36:56 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Miguel Rust Footer Module
+> Here is an example of it being used.
+```javascript
+  import {footer} from "miguel-first-npm-package";
+  footer("My Name");
+```
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+var moment = require('moment');
+exports.footer = function (name) {
+  return "Copyright "+moment().format('YYYY')+" "+name+" All Rights Reserved";
+}
